393923040be78bb0e394b5fdbfa5f9abaab2f83a
Radik <rgaliullin55@gmail.com>
2020-12-30 13:23:38 +0500

    add gulp to project

---
+++ gulpfile.js
@@ -0,0 +1,80 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const browserSync = require("browser-sync");
+const uglify = require("gulp-uglify");
+const concat = require("gulp-concat");
+const rename = require("gulp-rename");
+const autoprefixer = require("gulp-autoprefixer");
+const del = require("del");
+
+gulp.task("clean", async function () {
+  del.sync("dist");
+});
+
+gulp.task("scss", function () {
+  return gulp
+    .src("app/scss/**/*.scss")
+    .pipe(sass({ outputStyle: "compressed" }))
+    .pipe(
+      autoprefixer({
+        browsers: ["last 2 versions"],
+      })
+    )
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(gulp.dest("app/css"))
+    .pipe(browserSync.reload({ stream: true }));
+});
+
+gulp.task("css", function () {
+  return gulp
+    .src(["node_modules/normalize.css/normalize.css"])
+    .pipe(concat("_libs.scss"))
+    .pipe(gulp.dest("app/scss"))
+    .pipe(browserSync.reload({ stream: true }));
+});
+
+gulp.task("html", function () {
+  return gulp.src("app/*.html").pipe(browserSync.reload({ stream: true }));
+});
+
+gulp.task("js", function () {
+  return gulp
+    .src([])
+    .pipe(concat("libs.min.js"))
+    .pipe(uglify())
+    .pipe(gulp.dest("app/js"))
+    .pipe(browserSync.reload({ stream: true }));
+});
+
+gulp.task("browser-sync", function () {
+  browserSync.init({
+    server: {
+      baseDir: "app/",
+    },
+  });
+});
+
+gulp.task("export", function () {
+  const buildHtml = gulp.src("app/**/*.html").pipe(gulp.dest("dist"));
+
+  const buildCss = gulp.src("app/css/**/*.css").pipe(gulp.dest("dist/css"));
+
+  const buildJs = gulp.src("app/js/**/*.js").pipe(gulp.dest("dist/js"));
+
+  const buildFonts = gulp.src("app/fonts/**/*.*").pipe(gulp.dest("dist/fonts"));
+
+  const buildImg = gulp.src("app/img/**/*.*").pipe(gulp.dest("dist/img"));
+});
+
+gulp.task("watch", function () {
+  gulp.watch("app/scss/**/*.scss", gulp.parallel("scss"));
+  gulp.watch("app/*.html", gulp.parallel("html"));
+  gulp.watch("app/js/*.js", gulp.parallel("js"));
+});
+
+gulp.task("build", gulp.series("clean", "export"));
+
+gulp.task(
+  "default",
+  gulp.parallel("css", "scss", "js", "browser-sync", "watch")
+);
