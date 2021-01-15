e68f7a602f279a9a001e2bc5a0893cc445085040
Radik <rgaliullin55@gmail.com>
2021-01-15 19:20:48 +0500

    problem with axios has solved
    Problem with getting data from form an sending to server to response weather data. Add axios CDN package becouse require and import not work.

--- app/js/main.js
+++ app/js/main.js
@@ -1,35 +1,42 @@
-// const axios = require("axios");
-let search = document.getElementById("set");
-let currentCity = document.getElementById("root");
-search.addEventListener("submit", function (event) {
-  let inputText = event.target.querySelector(".header__search-input").value;
-  currentCity.textContent = inputText;
-  event.target.querySelector(".header__search-input").value = "";
+// import getData from "./axios";
+let search = document.querySelector("form");
+let currentCity = document.querySelector(".viewer__action");
+let city = "London";
+
+async function getData(city) {
+  try {
+    let userData = await axios({
+      method: "get",
+      baseURL: "https://api.openweathermap.org/data/2.5/weather",
+      responseType: "json",
+      params: {
+        method: "get",
+        APPID: "b9d6c3b529a4bdfda5ee9a6a843399af",
+        units: "metric",
+        lang: "ru",
+        q: `${city}`,
+      },
+    });
+
+    console.log(userData);
+    return (data = userData);
+  } catch (error) {
+    console.log(`Axios request failed:${error}`);
+  }
+}
+
+search.addEventListener("submit", (event) => {
   event.preventDefault();
+  let formData = new FormData(event.target);
+  const input = [...formData.values()];
+  console.log(input);
+  let city = input;
+
+  currentCity.textContent = input;
+
+  let data = getData(city);
+
+  console.log(data);
+
+  event.target.reset();
 });
-
-let myClicker = function (e) {
-  // async function getUser(city) {
-  //   try {
-  //     const response = await axios({
-  //       method: "get",
-  //       url: "https://api.openweathermap.org/data/2.5/weather?",
-  //       params: {
-  //         APPID: "b9d6c3b529a4bdfda5ee9a6a843399af",
-  //         units: "metric",
-  //         lang: "ru",
-  //         q: `${city}`,
-  //       },
-  //     });
-  //     console.log(response.data);
-  //     return (data = response.data);
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // }
-
-  console.log("sdgdsged");
-  alert(this.e.value);
-};
-
-//console.log(myClicker);
